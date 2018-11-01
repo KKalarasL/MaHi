c49f7b8122a44f92b6a874636884559fd27f6b7d
KKalarasL <konstantinos.kalaras@upstreamsystems.com>
2018-11-01 16:40:33 +0200

    Adding Home-Blog Pages

--- wp-content/themes/index.php
+++ wp-content/themes/index.php
@@ -221,6 +221,94 @@ Code -05- (Interion Page Template)
 
 // </div>
 
+Code -06- (Blog Page Template)
+/*
+<?php get_header();
+
+<div class="page-banner">
+	<div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);">
+	</div>
+	<div class="page-banner__content container container--narrow">
+	  <h1 class="page-banner__title">Welcome to our blog!</h1>
+	  <div class="page-banner__intro">
+	    <p>Keep up with our latest news.</p>
+	  </div>
+	</div>  
+</div>
+
+<div class="container container--narrow page-section">
+<?php
+	while(have_posts()) {
+		the_post(); ?>
+		<div class="post-item">
+			<h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+
+			<div class="metabox">
+				<p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> in <?php echo get_the_category_list(', '); ?></p>
+			</div>
+
+			<div class="generic-content">
+				<?php the_excerpt(); ?>
+				<p><a class="btn btn--blue" href="<?php the_permalink(); ?>">Continue reading &raquo;</a></p>
+			</div>
+
+		</div>
+	<?php }
+?>
+</div>
+
+<?php get_footer(); ?> 
+*/
+
+Code -07- (Single Post Template)
+/*
+<div class="page-banner">
+	<div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
+	<div class="page-banner__content container container--narrow">
+	  <h1 class="page-banner__title"><?php the_title(); ?></h1>
+	  <div class="page-banner__intro">
+	    <p>Don't forget to replace me later</p>
+	  </div>
+	</div>  
+</div>
+
+<div class="container container--narrow page-section">
+	<div class="metabox metabox--position-up metabox--with-home-link">
+		<p><a class="metabox__blog-home-link" href="<?php echo site_url('/blog'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Blog Home</a> <span class="metabox__main">Posted by <?php the_author_posts_link(); ?> on <?php the_time('j.n.Y'); ?> in <?php echo get_the_category_list(', '); ?></span></p>
+</div>
+
+	<div class="generic-content"><?php the_content(); ?></div>
+
+</div>
+*/
+
+Code -08- (Custom Queries)
+/*
+<h2 class="headline headline--small-plus t-center">From Our Blogs</h2>
+<?php
+  $homepagePosts = new WP_Query(array(
+    'posts_per_page' => 2
+  ));
+
+  while ($homepagePosts->have_posts()) {
+    $homepagePosts->the_post(); ?>
+
+<div class="event-summary">
+  <a class="event-summary__date event-summary__date--beige t-center" href="#">
+    <span class="event-summary__month"><?php the_time('M'); ?></span>
+    <span class="event-summary__day"><?php the_time('d'); ?></span>  
+  </a>
+  <div class="event-summary__content">
+    <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+    <p><?php echo wp_trim_words(get_the_content(), 18); ?><a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
+  </div>
+</div>
+<?php } wp_reset_postdata();
+?>
+*/
+
+<p class="t-center no-margin"><a href="<?php echo site_url('/blog'); ?>" class="btn btn--yellow">View All Blog Posts</a></p>
+
 Code -ZZ- (CSS Style)
 //
 // /* 3rd party packages */
